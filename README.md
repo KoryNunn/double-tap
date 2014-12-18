532863340bd63b5dec31f75c59801cc6f4b74ea4
mauricebutler <maurice.butler@gmail.com>
2014-12-18 13:33:36 +1000

    cleaned up, updated readme and added licence

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # double-tap
 
-pop a exit code in dat processes kill
+Pop a exit code in dat processes kill
 
-# API
+## API
 
 example:
 
     browserify ./sometest.js | double-tap smokestack
+
+
+## Why
+
+After running tests, if your test runner doesn't close this will ensure it goes down and stays down.
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 var args = process.argv,
-    childProcess = require('child_process');
-    finished = require('tap-finished');
+    childProcess = require('child_process'),
+    finished = require('tap-finished'),
+    child = childProcess.spawn(args[2], args.slice(3));
 
-var child = childProcess.spawn(args[2], args.slice(3));
 process.stdin.pipe(child.stdin);
+
 child.stdout.pipe(process.stdout);
 child.stderr.pipe(process.stderr);
 child.stdout.pipe(finished(function(results) {
